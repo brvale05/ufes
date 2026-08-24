0447eb1b8845cc676b0f1f4b1b867fc1372c5d41
brvale05 <brvale05@gmail.com>
2026-08-24 14:54:09 -0300

    ft: criação josephus problem

---
+++ tbo/lab01/josephus-problem/vector.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vector.h"
+
+struct Vector
+{
+    int *array;
+    int size;
+};
---
+++ tbo/lab01/josephus-problem/vector.h
@@ -0,0 +1,7 @@
+#ifndef VECTOR_H
+#define VECTOT_H
+
+typedef struct Vector Vector;
+typedef void* data_type;
+
+#endif
